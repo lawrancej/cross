4bafdfa4f0093bc95de1cff18eadd78b5ce8ce3c
Joey Lawrance <lawrancej@wit.edu>
2012-07-23 14:55:21 -0400

    Added MinGW package.

---
+++ packages/windows/mingw.ps1
@@ -0,0 +1,10 @@
+$package = @{
+    TITLE="MinGW"
+    DESCRIPTION="Minimalist GNU for Windows."
+    CHECK="C:\MinGW\bin\gcc.exe"
+    URL="http://sourceforge.net/projects/mingw/files/"
+    DEPENDS=@()
+    DOWNLOAD="http://sourceforge.net/projects/mingw/files/latest/download?source=files"
+    DESTFILE="mingw-get-inst.exe"
+}
+
